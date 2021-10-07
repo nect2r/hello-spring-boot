2f08f3c0202644b7fb5af8b3da0ebe0f7a50010c
nect2r <nect2r@kakao.com>
2021-10-07 17:03:09 +0900

    회원 리포지토리 로직 생성 및 테스트 케이스 작성
    -> 서비스 로직에 대한 이해
    -> Junit 으로 테스트 케이스 작성의 이해
    -> TDD(테스트 주도 개발) 의 기초 이해

---
+++ src/test/java/hello/hellospringboot/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,62 @@
+package hello.hellospringboot.repository;
+
+import hello.hellospringboot.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result =repository.findById(member.getId()).get();
+        Assertions.assertEquals(member, result);
+        org.assertj.core.api.Assertions.assertThat(member).isEqualTo(result);
+        System.out.println("result = " + (result == member));
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        org.assertj.core.api.Assertions.assertThat(member1).isEqualTo(result);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        org.assertj.core.api.Assertions.assertThat(result.size()).isEqualTo(2);
+    }
+}
