97586b03cea822bf110829d94c14b1091d4e6a43
nect2r <nect2r@kakao.com>
2021-10-10 23:02:13 +0900

    JdbcTemplate 로 변경해서 사용해보기

--- src/main/java/hello/hellospringboot/SpringConfig.java
+++ src/main/java/hello/hellospringboot/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospringboot;
 
 import hello.hellospringboot.repository.JdbcMemberRepository;
+import hello.hellospringboot.repository.JdbcTemplateMemberRepository;
 import hello.hellospringboot.repository.MemberRepository;
 import hello.hellospringboot.repository.MemoryMemberRepository;
 import hello.hellospringboot.service.MemberService;
@@ -28,7 +29,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
