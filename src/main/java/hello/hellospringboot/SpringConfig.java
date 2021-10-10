9a4a042124e7651afb3865a575d3f8788050ab7a
nect2r <nect2r@kakao.com>
2021-10-10 21:21:59 +0900

    H2 DB 연결 및 그래들 성정
    SpringConfig memberRepository 를 JdbcMemberRepository 로 수정
    로직은 변경되지 않지만 메모리 저장에서 DB저장으로 매우쉽게 수정

--- src/main/java/hello/hellospringboot/SpringConfig.java
+++ src/main/java/hello/hellospringboot/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospringboot;
 
+import hello.hellospringboot.repository.JdbcMemberRepository;
 import hello.hellospringboot.repository.MemberRepository;
 import hello.hellospringboot.repository.MemoryMemberRepository;
 import hello.hellospringboot.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
